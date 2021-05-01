8e7a01bfe4f52d3cea1d81b85853b1dcee76546b
Bibhushan <bibhushan.karki11@gmail.com>
2021-05-01 18:49:57 +0545

    loginpage buttons

--- Login.js
+++ Login.js
@@ -1,17 +1,22 @@
 import React,{useState,useEffect} from 'react'
-import {ImageBackground,View,Text,StyleSheet} from 'react-native'
+import {ImageBackground,View,Image,StyleSheet} from 'react-native'
 import {} from 'react-native'
+import GreenBtn from './app/components/GreenBtn';
 
 var splash=require('./assets/images/splash.png')
-var LoginBackground=require('./assets/images/Login.svg')
+var LoginBackground=require('./assets/images/Login.png')
+var logo=require('./assets/images/sajilologo.png');
 
-const Splash = () => {
+const Login = () => {
   const [show, setShow] = useState(true);
   useEffect(() => {
     setTimeout(() => {
       setShow(false);
     }, 1000);
   }, []);
+  const onPress = () => {
+    alert('clicked')
+  }
 
   return show ? 
     (<ImageBackground
@@ -19,9 +24,20 @@ const Splash = () => {
     style={{height:'100%',width:'100%'}}
     >
     </ImageBackground>) : (
-      <ImageBackground source={LoginBackground} style={{height:'100%',width:'100%'}}>
-        <View style={styles.conatiner}>
-          <Text style={styles.logBtn}>Login Screen</Text>
+      <ImageBackground source={LoginBackground} style={{height:'100%',width:'100%'}} >
+          <View >
+            <View style={{height:92,width:126}}>
+              <Image source={logo} style={{height:'100%',width:'100%',resizeMode: 'contain'}}></Image>
+            </View>
+            <GreenBtn
+              text='Log in'
+              type='outlined'
+              onPress={onPress}
+            />
+            <GreenBtn
+              text='Create Account'
+              onPress={onPress}
+            />
         </View>
       </ImageBackground>
     
@@ -29,12 +45,13 @@ const Splash = () => {
   
 }
 
-export default Splash
+export default Login
 const styles = StyleSheet.create({
   container: {
       flex: 1,
       alignItems:'center',
       justifyContent:'center',
+
   }, 
   logBtn:{
     fontSize:30
---
+++ app/components/GreenBtn/GreenBtn.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Text, View, TouchableOpacity, Dimensions } from 'react-native'
+
+
+const width = Dimensions.get('window').width
+
+const GreenBtn = ({ text, onPress,type = 'filled', bordered = false, size = 'large'  }) => {
+  const large = width / 1.3
+  const small = width / 2
+  const btnSize = size === 'large' ? large : small
+  const btnBgColor = type === 'filled' ? '#26B1B1' : '#fff'
+  const btnTextColor = type === 'filled' ? '#ffffff' : '#707070'
+  const btnBorderRadius = bordered ? 30 : 5
+
+
+  const containerCommonStyle = {
+    backgroundColor: btnBgColor,
+    paddingVertical: 8,
+    width: btnSize,
+    borderRadius: btnBorderRadius
+  }
+
+  const textCommonStyle = {
+    color: btnTextColor,
+    fontSize: 16,
+    textAlign: 'center',
+    fontFamily: 'Quicksand-Medium'
+  }
+  const border = type === 'outlined' && { borderColor: '#808080', borderWidth: 1 }
+
+  return (
+     <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+      <View style={[containerCommonStyle, border]}>
+        <Text style={[textCommonStyle]}> {text} </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default GreenBtn
+
