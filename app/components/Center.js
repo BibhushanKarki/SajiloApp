a77c7e130ce095820b681aa7d63b7f24ede02243
Bibhushan Karki <bibhushan.karki11@gmail.com>
2021-05-06 15:27:23 +0545

    Update Center.js

--- app/components/Center.js
+++ app/components/Center.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { View } from 'react-native'
 
-const Center = () => {
+const Center = ({props}) => {
   return (
     <View
       style={{
         flex:1,
         alignItems:"center",
         justifyContent:"center"
-      }}>
+      }}>{props.children}
     </View>
   )
 }
